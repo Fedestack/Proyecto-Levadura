7aae92e8c8121fe457dfd2bb66aadd50e4e2f10a
Fede Negreira <fedenegreira@gmail.com>
2025-07-15 01:36:38 -0300

    comenzando

---
+++ src/main/java/com/fedestack/Levadura/LevaduraApplication.java
@@ -0,0 +1,13 @@
+package com.fedestack.Levadura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevaduraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevaduraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fedestack/Levadura/LevaduraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fedestack.Levadura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevaduraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
